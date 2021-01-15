716be63324df70a5f7a7269cd820ae6f68efe765
santosh <santoshpandey878@gmail.com>
2021-01-15 19:15:16 +0530

    Initial commit

---
+++ subscriptionservice/src/main/java/com/gymondo/subscriptionservice/SubscriptionserviceApplication.java
@@ -0,0 +1,13 @@
+package com.gymondo.subscriptionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscriptionserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubscriptionserviceApplication.class, args);
+	}
+
+}
---
+++ subscriptionservice/src/test/java/com/gymondo/subscriptionservice/SubscriptionserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gymondo.subscriptionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubscriptionserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
